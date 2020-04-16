2e046ec2eb625f7558a54d931d5ef40d03fbedea
kanagavel <kanagu2win@gmail.com>
2020-04-16 18:25:39 +0530

    16-04-2020 6:25PM Files

--- lionsclub/src/main/java/com/lionsclub/springboot/thymeleaf/controller/HomeController.java
+++ lionsclub/src/main/java/com/lionsclub/springboot/thymeleaf/controller/HomeController.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,21 +23,16 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.lionsclub.springboot.thymeleaf.entity.Member;
 import com.lionsclub.springboot.thymeleaf.service.MemberService;
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
-
-
 
 @Controller
 public class HomeController {
-	
-	
+
 	@Autowired
 	private MemberService memberService;
 
 	// create a mapping for "/hello"
 
-	@GetMapping("/") 
+	@GetMapping("/")
 	public String home(Model theModel) {
 
 		theModel.addAttribute("theDate", new java.util.Date());
@@ -46,55 +40,54 @@ public class HomeController {
 		return "index";
 	}
 
-	@GetMapping("/index") 
+	@GetMapping("/index")
 	public String index(Model theModel) {
 
 		theModel.addAttribute("theDate", new java.util.Date());
 
 		return "index";
 	}
-	
+
 	@GetMapping("/internationallionsclub")
-	public String internationalLions()
-	{
+	public String internationalLions() {
 		return "internationlionssite";
 	}
-	
-	
+
 	@GetMapping("/memberedit")
-	public String memberadd(@RequestParam("id") int memberid, Model theModel ) {
-		
-		Member editmemberDetails=memberService.findById(memberid);
-		
+	public String memberadd(@RequestParam("id") int memberid, Model theModel) {
+
+		Member editmemberDetails = memberService.findById(memberid);
+
 		theModel.addAttribute("members", editmemberDetails);
 		theModel.addAttribute("savestatus", false);
-		
+
 		return "memberadd";
 	}
-	
+
 	@PostMapping("/memberedit")
-	public String membersave(HttpServletRequest request,  @ModelAttribute("members") Member member, @RequestParam("profilepicture") MultipartFile profilepicture, Model themodel) {
-		
+	public String membersave(HttpServletRequest request, @ModelAttribute("members") Member member,
+			@RequestParam("profilepicture") MultipartFile profilepicture, Model themodel) {
+
 		String uploadRootPath = request.getServletContext().getRealPath("profilepic");
-		 System.out.println("uploadRootPath=" + uploadRootPath);
-		 
-	      File uploadRootDir = new File(uploadRootPath);
-	      // Create directory if it not exists.
-	      if (!uploadRootDir.exists()) {
-	         uploadRootDir.mkdirs();
-	      }
-		
-		StringBuilder filename=new StringBuilder();
-		Path fileNameandPath =Paths.get(uploadRootPath,profilepicture.getOriginalFilename());
+		System.out.println("uploadRootPath=" + uploadRootPath);
+
+		File uploadRootDir = new File(uploadRootPath);
+		// Create directory if it not exists.
+		if (!uploadRootDir.exists()) {
+			uploadRootDir.mkdirs();
+		}
+
+		StringBuilder filename = new StringBuilder();
+		Path fileNameandPath = Paths.get(uploadRootPath, profilepicture.getOriginalFilename());
 		filename.append(profilepicture.getOriginalFilename());
-		
+
 		try {
-			Files.write(fileNameandPath,profilepicture.getBytes());
+			Files.write(fileNameandPath, profilepicture.getBytes());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		System.out.println(fileNameandPath);
 		member.setProfileImg("profilepic/" + filename);
 		memberService.save(member);
@@ -114,64 +107,190 @@ public class HomeController {
 
 		return "memberlist";
 	}
-	
+
 	@GetMapping("/membersearch")
-	public String memberIdsearch(@RequestParam("id") String memberid, Model theModel ) {
-		
-		List<Member> editmemberDetails=memberService.findByMemberID(memberid.toString());
-		
+	public String memberIdsearch(@RequestParam("id") String memberid, Model theModel) {
+
+		List<Member> editmemberDetails = memberService.findByMemberID(memberid.toString());
+
 		theModel.addAttribute("members", editmemberDetails.get(0));
 		theModel.addAttribute("savestatus", false);
-		
+
 		return "memberadd";
 	}
-	
+
 	@GetMapping("/memberupload")
-	public String memberUploadcsv()
-	{
-		
+	public String memberUploadcsv() {
+
+		return "memberuploadcsv";
+	}
+
+	@PostMapping("/memberupload")
+	public String memberUploadSavecsv(@RequestParam("file") MultipartFile file, Model model,
+			RedirectAttributes redirectAttributes) {
+
+		int newMemberIDdetailsCount = 0;
+		// validate file
+		if (file.isEmpty()) {
+			model.addAttribute("message", "Please select a CSV file to upload.");
+			model.addAttribute("status", false);
+		} else {
+
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+				String line = br.readLine(); // Reading header, Ignoring
+				while ((line = br.readLine()) != null && !line.isEmpty()) {
+
+					// line=line.replace("\"", "");
+					String[] fields= new String[49];
+					fields = line.split(",");
+					String memberIDcvs = fields[7].replace("\"", "");
+					// ------------------------------------------------------------------------------------
+					// ------------------------------------------------------------------------------------
+					// Split data from csv field level
+					String Multiple_District_Name = fields[0].replace("\"", "");
+					String District_Name = fields[1].replace("\"", "");
+					String Region_Name = fields[2].replace("\"", "");
+					String Zone_Name = fields[3].replace("\"", "");
+					String Title = fields[4].replace("\"", "");
+					String Club_ID = fields[5].replace("\"", "");
+					String Club_Name = fields[6].replace("\"", "");
+					String MemberID = fields[7].replace("\"", "");
+					String Prefix = fields[8].replace("\"", "");
+					String First_Name = fields[9].replace("\"", "");
+					String Middle_Name = fields[10].replace("\"", "");
+					String Last_Name = fields[11].replace("\"", "");
+					String Suffix = fields[12].replace("\"", "");
+					String Invalid_Member_Address_Flag = fields[13].replace("\"", "");
+					String Member_Address_Line_1 = fields[14].replace("\"", "");
+					String Member_Address_Line_2 = fields[15].replace("\"", "");
+					String Member_Address_Line_3 = fields[16].replace("\"", "");
+					String Member_Address_Line_4 = fields[17].replace("\"", "");
+					String Member_Address_City = fields[18].replace("\"", "");
+					String Member_Address_State = fields[19].replace("\"", "");
+					String Member_Address_Postal_Code = fields[20].replace("\"", "");
+					String Member_Address_Country = fields[21].replace("\"", "");
+					String Invalid_Officer_Address_Flag = fields[22].replace("\"", "");
+					String Officer_Address_Line_1 = fields[23].replace("\"", "");
+					String Officer_Address_Line_2 = fields[24].replace("\"", "");
+					String Officer_Address_Line_3 = fields[25].replace("\"", "");
+					String Officer_Address_Line_4 = fields[26].replace("\"", "");
+					String Officer_Address_City = fields[27].replace("\"", "");
+					String Officer_Address_State = fields[28].replace("\"", "");
+					String Officer_Address_Postal_Code = fields[29].replace("\"", "");
+					String Officer_Address_Country = fields[30].replace("\"", "");
+					String Email = fields[31].replace("\"", "");
+					String Home_Phone = fields[32].replace("\"", "");
+					String Cell_Phone = fields[33].replace("\"", "");
+					String Fax_Number = fields[34].replace("\"", "");
+					String Work_Phone = fields[35].replace("\"", "");
+					String Spouse_Name = fields[36].replace("\"", "");
+					String Membership_Type = fields[37].replace("\"", "");
+					String Date_of_Birth = fields[38].replace("\"", "");
+					String Gender = fields[39].replace("\"", "");
+					String Nick_Name = fields[40].replace("\"", "");
+					String Occupation = fields[41].replace("\"", "");
+					String Join_Date = fields[42].replace("\"", "");
+					String Life_Member = fields[43].replace("\"", "");
+					String Family_Unit = fields[44].replace("\"", "");
+					String Sponsor_Name = fields[45].replace("\"", "");
+					String Club_Branch_Name = fields[46].replace("\"", "");
+					String International_Discount = fields[47].replace("\"", "");
+					String International_Discount_Reason = "" ;
+					if (fields.length==49)
+					{
+					 International_Discount_Reason = fields[48].replace("\"", "");
+					}
+
+					// ------------------------------------------------------------------------------------
+					// ------------------------------------------------------------------------------------
+					Member newMember;
+					List<Member> editmemberDetails = memberService.findByMemberID(memberIDcvs);
+
+					
+
+					if (editmemberDetails.isEmpty()) {
+						newMember = new Member();
+						newMemberIDdetailsCount=newMemberIDdetailsCount+1;
+					} else {
+
+						newMember = editmemberDetails.get(0);
+					}
+
+					newMember.setMultiple_District_Name(Multiple_District_Name);
+					newMember.setDistrict_Name(District_Name);
+					newMember.setRegion_Name(Region_Name);
+					newMember.setZone_Name(Zone_Name);
+					newMember.setTitle(Title);
+					newMember.setClub_ID(Club_ID);
+					newMember.setClub_Name(Club_Name);
+					newMember.setMemberID(MemberID);
+					newMember.setPrefix(Prefix);
+					newMember.setFirst_Name(First_Name);
+					newMember.setMiddle_Name(Middle_Name);
+					newMember.setLast_Name(Last_Name);
+					newMember.setSuffix(Suffix);
+					newMember.setInvalid_Member_Address_Flag(Invalid_Member_Address_Flag);
+					newMember.setMember_Address_Line_1(Member_Address_Line_1);
+					newMember.setMember_Address_Line_2(Member_Address_Line_2);
+					newMember.setMember_Address_Line_3(Member_Address_Line_3);
+					newMember.setMember_Address_Line_4(Member_Address_Line_4);
+					newMember.setMember_Address_City(Member_Address_City);
+					newMember.setMember_Address_State(Member_Address_State);
+					newMember.setMember_Address_Postal_Code(Member_Address_Postal_Code);
+					newMember.setMember_Address_Country(Member_Address_Country);
+					newMember.setInvalid_Officer_Address_Flag(Invalid_Officer_Address_Flag);
+					newMember.setOfficer_Address_Line_1(Officer_Address_Line_1);
+					newMember.setOfficer_Address_Line_2(Officer_Address_Line_2);
+					newMember.setOfficer_Address_Line_3(Officer_Address_Line_3);
+					newMember.setOfficer_Address_Line_4(Officer_Address_Line_4);
+					newMember.setOfficer_Address_City(Officer_Address_City);
+					newMember.setOfficer_Address_State(Officer_Address_State);
+					newMember.setOfficer_Address_Postal_Code(Officer_Address_Postal_Code);
+					newMember.setOfficer_Address_Country(Officer_Address_Country);
+					newMember.setEmail(Email);
+					newMember.setHome_Phone(Home_Phone);
+					newMember.setCell_Phone(Cell_Phone);
+					newMember.setFax_Number(Fax_Number);
+					newMember.setWork_Phone(Work_Phone);
+					newMember.setSpouse_Name(Spouse_Name);
+					newMember.setMembership_Type(Membership_Type);
+					newMember.setDate_of_Birth(Date_of_Birth);
+					newMember.setGender(Gender);
+					newMember.setNick_Name(Nick_Name);
+					newMember.setOccupation(Occupation);
+					newMember.setJoin_Date(Join_Date);
+					newMember.setLife_Member(Life_Member);
+					newMember.setFamily_Unit(Family_Unit);
+					newMember.setSponsor_Name(Sponsor_Name);
+					newMember.setClub_Branch_Name(Club_Branch_Name);
+					newMember.setInternational_Discount(International_Discount);
+					newMember.setInternational_Discount_Reason(International_Discount_Reason);
+					memberService.save(newMember);
+
+					
+				}
+
+				br.close();
+			} catch (Exception e) {
+
+				System.out.println(e.getMessage());
+			}
+
+			model.addAttribute("status", true);
+			model.addAttribute("newMemberIDdetails", newMemberIDdetailsCount);
+
+		}
+
+		model.addAttribute("savestatus", false);
+
 		return "memberuploadcsv";
 	}
 	
-	@PostMapping("/memberupload")
-	public String memberUploadSavecsv(@RequestParam("file") MultipartFile file,Model model,RedirectAttributes redirectAttributes)
+	@GetMapping("MemberPendingInfo")
+	public String rptMemberPendingdetails(Model model)
 	{
-		
-        // validate file
-        if (file.isEmpty()) {
-            model.addAttribute("message", "Please select a CSV file to upload.");
-            model.addAttribute("status", false);
-        } else {
-
-            // parse CSV file to create a list of `User` objects
-            try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
-
-                // create csv bean reader
-                CsvToBean<Member> csvToBean = new CsvToBeanBuilder(reader)
-                        .withType(Member.class)
-                        .withIgnoreLeadingWhiteSpace(true)
-                        .build();
-
-                // convert `CsvToBean` object to list of users
-                List<Member> users = csvToBean.parse();
-
-                // TODO: save users in DB?
-
-                // save users list on model
-                model.addAttribute("users", users);
-                model.addAttribute("status", true);
-
-            } catch (Exception ex) {
-                model.addAttribute("message", "An error occurred while processing the CSV file.");
-                model.addAttribute("status", false);
-            }
-        }
-
-
-		
-		model.addAttribute("savestatus", false);
-		
-		return "memberuploadcsv";
+				
+		return "rptMemberPendingdetails";
 	}
 
 }
